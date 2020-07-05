3d4951f5885c73f23eb575873b5fdd428c0b6925
shan <1132649527@qq.con>
2020-07-05 09:58:42 +0800

    初始化

---
+++ src/views/Login/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+<h1>这是登录也面</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  created () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
